c0d2bfe6225ff4d12fc403200267a68bd258a844
Pencirik <magnabosco.federico@gmail.com>
2026-08-25 17:44:46 +0200

    Assets fire + fabric
    Added assets for fire and fabrics.
    Added shaders to the floor and walls.
    Added fire in the tutorial.
    
    Todo: fix the position of the walls

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -10,9 +10,14 @@ public class NewMonoBehaviourScript : MonoBehaviour
     public float textSpeed;
     private int index;
 
-    void Start()
+    // SOSTITUIAMO START CON ONENABLE: 
+    // In questo modo, ogni volta che un DialogueBox viene acceso (SetActive(true)), 
+    // partirà automaticamente il dialogo senza perdersi i tempi di Unity.
+    void OnEnable()
     {
-        textComponent.text = string.Empty;
+        if (textComponent != null)
+            textComponent.text = string.Empty;
+        
         StartDialogue();
     }
 
@@ -34,43 +39,36 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
     void StartDialogue() 
     {
-        // 1. Blocca il tempo di gioco (ferma il player, la fisica, ecc.)
         Time.timeScale = 0f;
-
         index = 0;
         StartCoroutine(TypeLine());
     }
 
     IEnumerator TypeLine()
-{
-    textComponent.text = string.Empty;
-    string currentLine = lines[index];
-    
-    int i = 0;
-    while (i < currentLine.Length)
     {
-        // Se troviamo l'inizio di un tag (es. "<sprite...")
-        if (currentLine[i] == '<')
-        {
-            // Cerchiamo la fine del tag '>'
-            int closingIndex = currentLine.IndexOf('>', i);
-            if (closingIndex != -1)
-            {
-                // Aggiungiamo l'intero tag al testo tutto in un colpo solo
-                textComponent.text += currentLine.Substring(i, (closingIndex - i) + 1);
-                // Spostiamo l'indice alla fine del tag
-                i = closingIndex + 1;
-                continue;
-            }
-        }
-
-        // Altrimenti, scriviamo il normale carattere uno alla volta
-        textComponent.text += currentLine[i];
-        i++;
+        textComponent.text = string.Empty;
+        string currentLine = lines[index];
         
-        yield return new WaitForSecondsRealtime(textSpeed);
+        int i = 0;
+        while (i < currentLine.Length)
+        {
+            if (currentLine[i] == '<')
+            {
+                int closingIndex = currentLine.IndexOf('>', i);
+                if (closingIndex != -1)
+                {
+                    textComponent.text += currentLine.Substring(i, (closingIndex - i) + 1);
+                    i = closingIndex + 1;
+                    continue;
+                }
+            }
+
+            textComponent.text += currentLine[i];
+            i++;
+            
+            yield return new WaitForSecondsRealtime(textSpeed);
+        }
     }
-}
 
     void NextLine()
     {
@@ -82,10 +80,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
         }
         else
         {
-            // 2. Quando il dialogo finisce, riattiva il tempo di gioco
             Time.timeScale = 1f;
-            
-            // 3. Disattiva il pannello del dialogo
             gameObject.SetActive(false);
         }
     }
