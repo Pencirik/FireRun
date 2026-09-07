848cd0a4dcef7a38f53e0d03c9efdc4f3123e22a
Pencirik <magnabosco.federico@gmail.com>
2026-09-07 17:24:15 +0200

    EndOfScene fix
    Fixed a bug in which the end of the scene was not correctly loaded

--- Assets/Scripts/EndDialogueScene.cs
+++ Assets/Scripts/EndDialogueScene.cs
@@ -1,37 +1,15 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 
 public class DialogueManager : MonoBehaviour
 {
     [Header("Configurazione Dialogo Finale")]
     public GameObject targetDialogueBox;
 
-#if UNITY_EDITOR
-    public SceneAsset endSceneAsset;
-#endif
-    private string endSceneName;
+    private string endSceneName = "MainMenu";
 
     private bool isMonitoring = false;
     private bool wasActive = false;
-
-#if UNITY_EDITOR
-    void OnValidate()
-    {
-        if (endSceneAsset != null)
-        {
-            string path = AssetDatabase.GetAssetPath(endSceneAsset);
-            int startIdx = path.LastIndexOf('/') + 1;
-            int endIdx = path.LastIndexOf('.');
-            if (endIdx > startIdx)
-            {
-                endSceneName = path.Substring(startIdx, endIdx - startIdx);
-            }
-        }
-    }
-#endif
 
     void Update()
     {
@@ -58,16 +36,13 @@ public class DialogueManager : MonoBehaviour
         isMonitoring = false;
         wasActive = false;
 
-        // Ripristina il tempo di gioco
-        Time.timeScale = 1f;
-
         if (!string.IsNullOrEmpty(endSceneName))
         {
             SceneManager.LoadScene(endSceneName);
         }
         else
         {
-            Debug.LogWarning("Nessuna scena finale selezionata nel DialogueManager!");
+            Debug.LogWarning("Nessuna scena finale impostata nel DialogueManager!");
         }
     }
 }
